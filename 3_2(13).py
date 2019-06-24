3fe9c4654fd892be7258f4a63a89eaef489c6325
adolf777hipster <protesting777@gmail.com>
2019-06-24 09:54:53 +0300

    lesson 3_2 task 13

---
+++ 3_2(13).py
@@ -0,0 +1,49 @@
+import unittest
+from selenium import webdriver
+import time
+
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # name
+        browser.find_element_by_xpath('//div//*[@placeholder = "Введите имя"]').send_keys('Name')
+        # second_name
+        browser.find_element_by_xpath('//div//*[@placeholder = "Введите фамилию"]').send_keys(
+            'Second name')
+        # email
+        browser.find_element_by_xpath('//div//*[@placeholder = "Введите Email"]').send_keys('email@gmail.com')
+        # button
+        browser.find_element_by_css_selector("button.btn").click()
+
+        time.sleep(1)
+
+        self.assertEqual("Поздравляем! Вы успешно зарегистировались!", browser.find_element_by_tag_name("h1").text,
+                         "Should be equal")
+
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # name
+        browser.find_element_by_xpath('//div//*[@placeholder = "Введите имя"]').send_keys('Name')
+        # second_name
+        browser.find_element_by_xpath('//div//*[@placeholder = "Введите фамилию"]').send_keys(
+            'Second name')
+        # email
+        browser.find_element_by_xpath('//div//*[@placeholder = "Введите Email"]').send_keys('email@gmail.com')
+        # button
+        browser.find_element_by_css_selector("button.btn").click()
+
+        time.sleep(1)
+
+        self.assertEqual("Поздравляем! Вы успешно зарегистировались!", browser.find_element_by_tag_name("h1").text,
+                         "Should be equal")
+
+
+if __name__ == "__main__":
+    unittest.main()
