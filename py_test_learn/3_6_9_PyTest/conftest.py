f75f148197258474391470906f061f4956f009fe
adolf777hipster <protesting777@gmail.com>
2019-07-01 11:53:17 +0300

    pytest 3_6_9 run test for different languages

---
+++ py_test_learn/3_6_9_PyTest/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: en or fr")
+
+
+# For Chrome
+@pytest.fixture(scope="function")
+def browser(request):
+    language_name = request.config.getoption("language")
+    if language_name == "es":
+        print('\nStart browser with English language')
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
+        browser = webdriver.Chrome(options=options)
+
+    elif language_name == "fr":
+        print('\nStart browser with French language')
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': "fr"})
+        browser = webdriver.Chrome(options=options)
+    else:
+        print("Language {} still is not implemented".format(language_name))
+    browser.implicitly_wait(1)
+    yield browser
+    print("\nQuit browser!")
+    browser.quit()
---
+++ py_test_learn/3_6_9_PyTest/test_items.py
@@ -0,0 +1,18 @@
+import time
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_button_add_to_trash(browser):
+    browser.get(link)
+    time.sleep(30)
+    browser.find_element_by_id("add_to_basket_form").click()
+    result = browser.find_element_by_class_name('alertinner').text
+    if result == "Coders at Work has been added to your basket.":
+        print("You  entered ENGLISH language with command string -->  pytest --language=es test_items.py ")
+    elif result == "Coders at Work a été ajouté à votre panier.":
+        print("You  entered FRENCH language with command string -->   pytest --language=fr test_items.py ")
+    else:
+        print(
+            "Sorry, but You  entered incorrect language check out your command for start test they must be:"
+            "\npytest --language=es test_items.py for ENGLISH"
+            "\npytest --language=fr test_items.py for FRENCH")
