52566a5fac806fa1b544ca7cdc682438a5fb3702
adolf777hipster <51943869+adolf777hipster@users.noreply.github.com>
2019-07-02 14:44:28 +0300

    Update conftest.py

--- py_test_learn/3_6_9_PyTest/conftest.py
+++ py_test_learn/3_6_9_PyTest/conftest.py
@@ -13,20 +13,9 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     language_name = request.config.getoption("language")
-    if language_name == "es":
-        print('\nStart browser with English language')
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
-        browser = webdriver.Chrome(options=options)
-
-    elif language_name == "fr":
-        print('\nStart browser with French language')
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': "fr"})
-        browser = webdriver.Chrome(options=options)
-    else:
-        print("Language {} still is not implemented".format(language_name))
-    browser.implicitly_wait(1)
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nQuit browser!")
     browser.quit()
